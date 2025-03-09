b596615c4ce4b4dcfc7e2888d68283bcf067824e
Flynn <flynn@Flynns-MacBook-Pro-3.local>
2025-03-09 03:45:51 -0400

    Cleaned up code

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,18 +1,20 @@
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 public class Chunk : MonoBehaviour {
+
     public static int CHUNK_SIZE = 32;
+    public static int CHUNK_SIZE_BIT_SHIFT = 5; // To save runtime, manually calculate
 
-    Material material;
-    Vector3Int chunkCoord;
     int[,,] chunk_data = new int[CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE];
+    Vector3Int chunk_coord;
 
-    public void Init(Vector3Int chunkCoord_in, Material material_in) {
-        chunkCoord = chunkCoord_in;
-        material = material_in;
+    public void Init(Vector3Int chunk_coord_in) {
+        chunk_coord = chunk_coord_in;
         GenerateChunk();
     }
 
@@ -20,9 +22,12 @@ public class Chunk : MonoBehaviour {
         for (int x = 0; x < CHUNK_SIZE; ++x) {
             for (int z = 0; z < CHUNK_SIZE; ++z) {
 
+                float ground_height = 120;
                 for (int y = 0; y < CHUNK_SIZE; ++y) {
-                    chunk_data[x, y, z] = 1;
+                    if (y + chunk_coord.y < ground_height) { chunk_data[x, y, z] = 1; }
+                    else { chunk_data[x, y, z] = 0; }
                 }
+
             }
         }
     }
@@ -47,10 +52,8 @@ public class Chunk : MonoBehaviour {
                         int nbr_x = x + VoxelData.directions[face].x;
                         int nbr_y = y + VoxelData.directions[face].y;
                         int nbr_z = z + VoxelData.directions[face].z;
-
-                        if (0 <= nbr_x && nbr_x < 32 && 0 <= nbr_y && nbr_y < 32 && 0 <= nbr_z && nbr_z < 32) {
-                            if (chunk_data[x + VoxelData.directions[face].x, y + VoxelData.directions[face].y, z + VoxelData.directions[face].z] == 1) { continue; }
-                        }
+                        Vector3Int nbr = new Vector3Int(nbr_x, nbr_y, nbr_z);
+                        if (GetLocalBlockType(nbr) == 1) { continue; }
 
                         for (int tri_idx = 0; tri_idx < 6; ++tri_idx) {
                             int vrtx_idx = VoxelData.Triangles[face, tri_idx];
@@ -72,6 +75,23 @@ public class Chunk : MonoBehaviour {
         mesh.uv = uvs.ToArray();
         mesh.triangles = triangles.ToArray();
         gameObject.GetComponent<MeshFilter>().mesh = mesh;
-        gameObject.GetComponent<MeshRenderer>().material = material;
+        gameObject.GetComponent<MeshRenderer>().material = World.material;
+    }
+
+    static bool LocalPositionIsInChunk(Vector3Int position) {
+        return 0 <= position.x && position.x < CHUNK_SIZE && 
+               0 <= position.y && position.y < CHUNK_SIZE &&
+               0 <= position.z && position.z < CHUNK_SIZE;
+    }
+
+    public Vector3Int GetGlobalPosition(Vector3Int position) {
+        return (chunk_coord * CHUNK_SIZE) + position;
+    }
+
+    public int GetLocalBlockType(Vector3Int position) {
+        if (LocalPositionIsInChunk(position)) return chunk_data[position.x, position.y, position.z];
+        return 0;
+
+        // return World.GetGlobalBlockType(GetGlobalPosition(position));
     }
 }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -10,28 +10,73 @@ using UnityEngine.UIElements;
 [RequireComponent(typeof(MeshRenderer))]
 public class World : MonoBehaviour {
 
-    public Material material;
-    Dictionary<int, Chunk> chunks;
+    public static int WORLD_HEIGHT_IN_CHUNKS = 1;
+    public static int WORLD_WIDTH_IN_CHUNKS = 4;
+    public static int WORLD_HEIGHT { get { return WORLD_HEIGHT_IN_CHUNKS << Chunk.CHUNK_SIZE_BIT_SHIFT; } }
+    public static int WORLD_WIDTH { get { return WORLD_WIDTH_IN_CHUNKS << Chunk.CHUNK_SIZE_BIT_SHIFT; } }
+    public static int HALF_WORLD_HEIGHT_IN_CHUNKS { get { return WORLD_HEIGHT_IN_CHUNKS >> 1; } }
+    public static int HALF_WORLD_WIDTH_IN_CHUNKS { get { return WORLD_WIDTH_IN_CHUNKS >> 1; } }
+    public static int HALF_WORLD_HEIGHT { get { return WORLD_HEIGHT >> 1; } }
+    public static int HALF_WORLD_WIDTH { get { return WORLD_WIDTH >> 1; } }
+
+    // We should always keep the dictionary position as the default, these should be a temporary use
+    public static Vector3Int GetChunkReadablePosition(Vector3Int position) { return position - new Vector3Int(HALF_WORLD_WIDTH_IN_CHUNKS, 0, HALF_WORLD_WIDTH_IN_CHUNKS); }
+    public static Vector3Int GetChunkWorldPosition(Vector3Int position) { return GetChunkReadablePosition(position) * new Vector3Int(Chunk.CHUNK_SIZE, 0, Chunk.CHUNK_SIZE); }
+    public static int GetChunkHash(Vector3Int position) { return Tuple.Create(position.x, position.y, position.z).GetHashCode(); }
+
+    public static Dictionary<int, Chunk> chunks = new Dictionary<int, Chunk>();
+    public static Material material;
+
 
     void Start() {
 
-        int y = 0;
-        for (int x = 0; x < 4; ++x) {
-            for (int z = 0; z < 4; ++z) {
+        material = Resources.Load<Material>("BlockTextures");
 
-                GameObject curr_chunk = new GameObject("chunk (" + x + " " + y + " " + z + ")");
-                curr_chunk.transform.position = new Vector3(x * Chunk.CHUNK_SIZE, y, z * Chunk.CHUNK_SIZE);
+        for (int y = 0; y < WORLD_HEIGHT_IN_CHUNKS; ++y) {
+            for (int x = 0; x < WORLD_WIDTH_IN_CHUNKS; ++x) {
+                for (int z = 0; z < WORLD_WIDTH_IN_CHUNKS; ++z) {
+                    
+                    GameObject curr_chunk = new GameObject("chunk ("+x+" "+y+" "+z+")");
+                    Vector3Int curr_coords = new Vector3Int(x, y, z);
 
-                curr_chunk.AddComponent<MeshFilter>();
-                curr_chunk.AddComponent<MeshRenderer>();
-                curr_chunk.AddComponent<Chunk>();
-                curr_chunk.GetComponent<Chunk>().Init(new Vector3Int(x, y, z), material);
+                    curr_chunk.transform.position = GetChunkWorldPosition(curr_coords);
+                    curr_chunk.AddComponent<MeshFilter>();
+                    curr_chunk.AddComponent<MeshRenderer>();
+                    curr_chunk.AddComponent<Chunk>();
 
-                curr_chunk.GetComponent<Chunk>();
-
-                chunks[Tuple.Create(x, y, z).GetHashCode()] = curr_chunk.GetComponent<Chunk>();
+                    Chunk curr_component = curr_chunk.GetComponent<Chunk>();
+                    curr_component.Init(curr_coords);
+                    chunks[GetChunkHash(curr_coords)] = curr_component;
+                }
             }
         }
-
     }
+
+    public static bool ChunkIsInWorld(Vector3Int position) {   
+        return 0 <= position.x && position.x < WORLD_WIDTH_IN_CHUNKS && 
+               0 <= position.y && position.y < WORLD_HEIGHT_IN_CHUNKS &&
+               0 <= position.z && position.z < WORLD_WIDTH_IN_CHUNKS;
+    }
+
+    public static Vector3Int GetChunkPosition(Vector3Int position) {
+        int x = position.x < 0 ? Mathf.CeilToInt(position.x / Chunk.CHUNK_SIZE) : Mathf.FloorToInt(position.x / Chunk.CHUNK_SIZE);
+        int y = position.x < 0 ? Mathf.CeilToInt(position.y / Chunk.CHUNK_SIZE) : Mathf.FloorToInt(position.y / Chunk.CHUNK_SIZE);
+        int z = position.x < 0 ? Mathf.CeilToInt(position.z / Chunk.CHUNK_SIZE) : Mathf.FloorToInt(position.z / Chunk.CHUNK_SIZE);
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int GetLocalPosition(Vector3Int position) {
+        return new Vector3Int(position.x % Chunk.CHUNK_SIZE, 
+                              position.y % Chunk.CHUNK_SIZE, 
+                              position.z % Chunk.CHUNK_SIZE);
+    }
+    
+
+    public static int GetGlobalBlockType(Vector3Int position) {
+        Vector3Int chunk_coord = GetChunkPosition(position);
+        if (!ChunkIsInWorld(chunk_coord)) return 0;
+        return chunks[Tuple.Create(chunk_coord.x, chunk_coord.y, chunk_coord.z).GetHashCode()]
+                   .GetLocalBlockType(GetLocalPosition(position));
+    }
+
 }
